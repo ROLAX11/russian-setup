b71c71c62a7c8a3cc46d6dab57cb5d4fbaf3b5ea
Priton Dev <priton@gmail.com>
2025-05-09 01:20:00 +0800

    Update: улучшения дизайна

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory, abort, render_template
+import os
 
 app = Flask(__name__)
 
@@ -12,3 +13,7 @@ def tutor_photo(name):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
